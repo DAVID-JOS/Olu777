b19ee350105ea52b228e7df609641d7af243f35a
DAVID-JOS <oluwayimikadavid2@gmail.com>
2025-09-14 23:11:44 +0100

    Create config.js
    Backend

---
+++ config.js
@@ -0,0 +1,9 @@
+require("dotenv").config(); // load .env
+
+module.exports = {
+  moniepointAPIKey: process.env.MONIEPOINT_API_KEY,
+  moniepointSecret: process.env.MONIEPOINT_SECRET,
+  usdToNGN: Number(process.env.USD_TO_NGN) || 460,
+  davCoinValueUSD: Number(process.env.DAVCOIN_VALUE_USD) || 10000,
+  port: process.env.PORT || 5000
+};
